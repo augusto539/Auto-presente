58d5ae0b01b963e0e3b6a07d76fa10276b0d8906
augusto539 <61762968+augusto539@users.noreply.github.com>
2020-08-06 18:42:09 -0300

    Add files via upload
    # se cambio el sistema de lectura para que lea de a un mensaje a la vez en un intervalo de tiempo especifico. por lo tanto no hace falta borrar el historial entre usos
    # se mejoro el orden del código
    # se agrego una opción a la interfaz (se relaciona con el primer punto)
    # se arreglo error en el timer de espera, ahora realiza la comprobación de hora actual cada 30 mins lo que mejora considerablemente el rendimiento en espera

--- functions.py
+++ functions.py
@@ -3,7 +3,42 @@ from selenium.webdriver.common.by import By
 from datetime import datetime
 import time 
 
-def main(name,message,hour):
+def hora_actual():
+    current_time = datetime.strftime(datetime.now(),"%H:%M:%S") #output: hora actual del pc
+
+    ahora = current_time.split(':')
+    a = int(ahora[0] + ahora[1])
+
+    return a
+def HORA_1(s_hora_1):
+    mytime = s_hora_1
+
+    hora = mytime.split(':')
+    b = int(hora[0] + hora[1])
+
+    return b
+def HORA_2(s_hora_2):
+    mytime = s_hora_2
+
+    hora = mytime.split(':')
+    b = int(hora[0] + hora[1])
+
+    return b
+
+
+
+def send(name_box,message_box,hour_1_box,hour_2_box):
+    name = name_box.get()
+    message = message_box.get()
+    s_hora_1 = hour_1_box.get()
+    s_hora_2 = hour_2_box.get()
+
+    hora_1 = HORA_1(s_hora_1)
+    hora_2 = HORA_2(s_hora_2)
+
+    main(name,message,hora_1,hora_2)
+
+def main(name,message,hora_1,hora_2):
     driver = webdriver.Chrome("chromedriver.exe")
     driver.get('https://web.whatsapp.com/')
 
@@ -14,31 +49,41 @@ def main(name,message,hour):
 
     msg_box = driver.find_element_by_class_name('_3uMse')
 
-    timer(hour)
+    ahora = hora_actual()
 
-    a = driver.find_elements_by_class_name('eRacY')
+    timer(hora_1,ahora)
 
-    comprobacion(a,msg_box,message,driver)
-
-
-def timer(hour):
-    hora_actual = datetime.now().time()
-    hora = datetime.strptime(f"{hour}:00:00", "%X").time() 
-
-    if hora_actual < hora:
-        while hora_actual < hora:
-            hora_actual = datetime.now().time()
-            time.sleep(1800)
+    validacion(msg_box,message,driver,hora_1,hora_2,ahora)
 
 
-def comprobacion(a,msg_box,message,driver):
-    for e in a:
-        b = e.find_element(By.TAG_NAME, 'span')
-        element = b.find_element(By.TAG_NAME, 'span')
-        if "presente" in element.text or "Presente" in element.text:
-            if "no" in element.text:
+def timer(hora_1,ahora):
+    
+    while ahora < hora_1:
+        time.sleep(1800)
+        ahora = hora_actual()
+        
+def validacion(msg_box,message,driver,hora_1,hora_2,ahora):
+    num = 0
+    while ahora < hora_2:
+        time.sleep(1)
+        ahora = hora_actual()
+
+        a = driver.find_elements_by_class_name('eRacY')
+        b = a[-1].find_element(By.TAG_NAME, 'span')
+        c = b.find_element(By.TAG_NAME, 'span')
+        element = c.text
+
+        num = num + 1
+
+        print(f"( {element} ) buelta n*{num}")
+
+        if "presente" in element or "Presente" in element or "PRESENTE":
+            if "no" in element:
                 break
             msg_box.send_keys(message)
             button = driver.find_element_by_class_name('_1U1xa')
             button.click()
-            break
+            #break
+
+        #time.sleep(180)
+
--- gui.py
+++ gui.py
@@ -1,14 +1,10 @@
 import tkinter
-from functions import main
+from functions import send
+from datetime import datetime
 
-def send():
-    name = name_box.get()
-    message = message_box.get()
-    hour = hour_box.get()
-    main(name,message,hour)
 
 window = tkinter.Tk()
-window.geometry('250x200')
+window.geometry('255x300')
 window.resizable(0,0)
 window.configure(bg="#263D42")
 window.title('Auto presente')
@@ -17,22 +13,29 @@ window.iconbitmap('icon.ico')
 frame = tkinter.Frame(window, bg="#263D42")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
 
-name_label = tkinter.Label(frame, text = "nombre del cotacto o grupo:")
+name_label = tkinter.Label(frame, text = "nombre del cotacto o grupo:",font = "helvetica 13")
 name_label.pack()
-name_box = tkinter.Entry(frame, font = "helvetica 10", justify = "center")
+name_box = tkinter.Entry(frame, font = "helvetica 13", justify = "center")
 name_box.pack()
 
-message_label = tkinter.Label(frame, text = "mensaje a enviar:")
+message_label = tkinter.Label(frame, text = "mensaje a enviar:",font = "helvetica 13")
 message_label.pack()
-message_box = tkinter.Entry(frame, font = "helvetica 10", justify ="center")
+message_box = tkinter.Entry(frame, font = "helvetica 13", justify ="center")
 message_box.pack()
 
-hour_label = tkinter.Label(frame, text = "hora a enviar:")
-hour_label.pack()
-hour_box = tkinter.Entry(frame,font = "helvetica 10", justify = "center")
-hour_box.pack()
+hour_1_label = tkinter.Label(frame, text = "hora de inicio:",font = "helvetica 13")
+hour_1_label.pack()
+hour_1_box = tkinter.Entry(frame,font = "helvetica 13", justify = "center")
+hour_1_box.insert(3, ":")
+hour_1_box.pack()
 
-send_buttom = tkinter.Button(frame, text = "programar",command=send)
+hour_2_label = tkinter.Label(frame, text = "hora final",font = "helvetica 13")
+hour_2_label.pack()
+hour_2_box = tkinter.Entry(frame,font = "helvetica 13", justify = "center")
+hour_2_box.insert(3, ":")
+hour_2_box.pack()
+
+send_buttom = tkinter.Button(frame, text = "programar",font = "helvetica 13",command= lambda: send(name_box,message_box,hour_1_box,hour_2_box))
 send_buttom.pack()
 
 window.mainloop()
